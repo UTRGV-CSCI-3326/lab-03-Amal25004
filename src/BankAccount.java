222acf43ac31371bb26b505984d78b52937699bb
A. Mal <jmaldonado250@icloud.com>
2025-02-02 05:13:10 +0000

    First commit. Outputs Bob's balances.

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,27 @@
+public class BankAccount
+{
+    public static void main (String[] args)
+    {
+        double balance = 2175.37;
+        System.out.println("Bob's balance is: " + balance);
 
+        balance -= 302.50;
+        System.out.println("Bob's new balance is: " + balance);
+
+        balance -= 50.03;
+        System.out.println("Bob's new balance is: " + balance);
+
+        balance /= 2;
+        balance += 20;
+        System.out.println("Bob's new balance is: " + balance);
+
+        balance -= 1;
+        System.out.println("Bob's new balance is: " + balance);
+
+        balance *= 2;
+        System.out.println("Bob's new balance is: " + balance);
+
+        balance += 1;
+        System.out.println("Bob's new balance is: " + balance);
+    }
+}
